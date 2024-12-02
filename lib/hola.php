2cd5631678819bd04d2c48793268854474b2fd76
Jordi Pozo <jpozoc@gmail.com>
2024-12-03 00:18:13 +0100

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,6 @@
 <?php
 // Autor: Jordi Pozo <jpozoc@gmail.com>
 // El nombre por defecto es Mundo
-// -----
+print "Introduce tu nombre";
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola {$nombre}\n";
